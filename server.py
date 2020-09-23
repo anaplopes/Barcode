233ae2aa047cf1bc6a5b4c12e6ba58673a7d2d2f
Ana Paula Lopes <30847848+anaplopes@users.noreply.github.com>
2020-09-23 17:02:49 -0300

    .

--- server.py
+++ server.py
@@ -2,12 +2,15 @@
 from core.app import create_app as app
 
 
-if __name__ == '__main__':
-    ip = '0.0.0.0'
-    port = app.config['APP_PORT']
-    debug = app.config['DEBUG']
+# if __name__ == '__main__':
+#     ip = '0.0.0.0'
+#     port = app.config['APP_PORT']
+#     debug = app.config['DEBUG']
     
-    # executa o servidor web do flask
-    app.run(
-        host=ip, debug=debug, port=port, use_reloader=debug
-    )
+#     # executa o servidor web do flask
+#     app.run(
+#         host=ip, debug=debug, port=port, use_reloader=debug
+#     )
+
+if __name__ == '__main__':
+    app.run()
