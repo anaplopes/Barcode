c1cd0d7d3eb813ee79c0d4147c449a2aae79a61a
Ana Paula Lopes <30847848+anaplopes@users.noreply.github.com>
2020-09-25 14:17:02 -0300

    removido form.reset, devido erro apresentado

--- core/routes.py
+++ core/routes.py
@@ -9,17 +9,16 @@ bp = Blueprint('barcode', __name__, url_prefix='/')
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
-    form = BarcodeForm(request.form)
+    form = BarcodeForm()
     service = BarcodeService()
-    del_zip = service.deleteZip(filename='core/kit.zip')
-    cls_dir = service.clearDir(dir='code/')
+    del_zip = service.deleteZip(filename='core/codes.zip')
+    cls_dir = service.clearDir(dir='core/code_image/')
     
     if request.method == 'POST' and form.validate_on_submit():
         code = request.form['codetype']
         text = True if request.form['includetext'] == 's' else False
         file = request.files['file']
         create_code = service.createImage(file=file, barcode_type=code, includetext=text)
-        create_zip = service.createZipFile(ziplocal='core/kit.zip', dirimage='code/')
-        form.reset()
-        return send_file('kit.zip', mimetype='application/zip', attachment_filename='kit.zip', as_attachment=True)
+        create_zip = service.createZipFile(ziplocal='core/codes.zip', dirimage='core/code_image/')
+        return send_file('codes.zip', mimetype='application/zip', attachment_filename='codes.zip', as_attachment=True)
     return render_template('index.html', form=form)
